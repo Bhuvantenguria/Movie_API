3b742c2b524741363416a41584bfa1faea0322b4
Bhuvan <100340704+Bhuvantenguria@users.noreply.github.com>
2025-02-27 22:10:22 +0530

    Solve the CORS error problem

--- src/main/java/com/cinema/auth/config/SecurityConfiguration.java
+++ src/main/java/com/cinema/auth/config/SecurityConfiguration.java
@@ -32,7 +32,7 @@ public class SecurityConfiguration {
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http.csrf(AbstractHttpConfigurer::disable)
 		.authorizeHttpRequests(auth-> auth
-				.requestMatchers("/api/v1/auth/**")
+				.requestMatchers("/api/v1/auth/**", "/api/v1/movie/**")
 				.permitAll()
 				.anyRequest()
 				.authenticated())
